240885f30df9cd4d151bb70b808c88043ba8e230
Nishant Gupta <nishant.bit@outlook.com>
2026-01-10 13:27:38 +0530

    chore: clean folder structure and remove generated JS files

--- server/src/lib/prisma.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-var client_1 = require("@prisma/client");
-var prisma = new client_1.PrismaClient();
-exports.default = prisma;
